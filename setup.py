88598fee3f77e0fedb0a392170890c2b559644ca
pa1vasanth <pavanvasanth33@gmail.com>
2022-03-07 21:29:40 +0000

    commit2

---
+++ setup.py
@@ -0,0 +1,13 @@
+
+from setuptools import setup, find_packages
+
+setup(
+    name='project0',
+    version='1.0',
+    author='PAVAN VASANTH KOMMINENI',
+    author_email='pa1vasanth@ou.edu',
+    packages=find_packages(exclude=('tests', 'docs')),
+    setup_requires=['pytest-runner'],
+    tests_require=['pytest']
+)
+
